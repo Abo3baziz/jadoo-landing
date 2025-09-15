a9add4c014406328b025b898d820d4d0cc40e9a4
Ahmed Aziz <azez55.ali@gmail.com>
2025-09-15 21:37:22 +0300

    feat: add hero section

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,5 +1,8 @@
 import decore from "../assets/svgs/Decore-2.svg";
 import NavBar from "../components/NavBar";
+import travelar from "../assets/images/Traveller 1.png";
+import playButton from "../assets/svgs/play-button.svg";
+import underlineDecore from "../assets/svgs/Decore.svg";
 
 function Home() {
   return (
@@ -8,10 +11,67 @@ function Home() {
         <img
           src={decore}
           alt="decore"
-          className="absolute top-0 right-0 size-150 -z-1"
+          className="absolute top-0 right-0 size-200 -z-1"
         />
 
         <NavBar />
+
+        <div className="grid grid-flow-col grid-col-2">
+          <div className="mt-16 -mr-[10rem]">
+            <p className="font-poppins font-bold text-[1.25rem] uppercase text-orange">
+              Best Destinations around the world
+            </p>
+
+            <p className="font-volkhov text-[5.25rem] text-purple w-fit tracking-tight">
+              Travel,{" "}
+              <p className="inline relative">
+                {/* 
+                TODO change image to SVG componenet using svgr 
+                TODO animate SVG
+                */}
+                <img
+                  className="absolute top-22 left-0 -z-1"
+                  src={underlineDecore}
+                  alt="decore image"
+                />{" "}
+                enjoy
+              </p>{" "}
+              <br />
+              and live a new <br /> and full life
+            </p>
+            <p className="font-poppins font-medium text-[#5E6282] mb-10">
+              Built Wicket longer admire do barton vanity itself do in it.{" "}
+              <br />
+              Preferred to sportsmen it engrossed listening. Park gate <br />{" "}
+              sell they west hard for the.
+            </p>
+            <div className="flex">
+              <button className="bg-[#F1A501] text-white rounded-[10px] w-[170px] h-[60px] mr-10">
+                Find out more
+              </button>
+
+              <div className="flex items-center gap-x-5">
+                <button>
+                  <img
+                    className="size-[52px]"
+                    src={playButton}
+                    alt="play button icon"
+                  />
+                </button>
+                <p className="text-[#686D77] font-poppins font-medium">
+                  Play Demo
+                </p>
+              </div>
+            </div>
+          </div>
+          <div>
+            <img
+              className="w-[55rem] relative "
+              src={travelar}
+              alt="travelar image"
+            />
+          </div>
+        </div>
       </div>
     </>
   );
