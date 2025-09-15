fd58f07e0b47b8b84ead42ff585ba5e68cc17771
Ahmed Aziz <azez55.ali@gmail.com>
2025-09-16 01:00:58 +0300

    feat: add SectionHeading and SectionName components

---
+++ src/shared/SectionHeading.jsx
@@ -0,0 +1,9 @@
+function SectionHeading({ text }) {
+  return (
+    <h1 className="font-volkhov text-var-purple-midnight text-[50px]">
+      {text}
+    </h1>
+  );
+}
+
+export default SectionHeading;
---
+++ src/shared/SectionName.jsx
@@ -0,0 +1,9 @@
+function SectionName({ text }) {
+  return (
+    <p className="uppercase font-poppins font-semibold text-[#5E6282]">
+      {text}
+    </p>
+  );
+}
+
+export default SectionName;
