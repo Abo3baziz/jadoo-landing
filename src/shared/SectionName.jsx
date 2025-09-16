bbba51630be865865f099b8454ef40f5260e68f3
Ahmed Aziz <azez55.ali@gmail.com>
2025-09-16 14:00:05 +0300

    feat: add condetional uppercase styling

--- src/shared/SectionName.jsx
+++ src/shared/SectionName.jsx
@@ -1,9 +1,10 @@
-function SectionName({ text }) {
-  return (
-    <p className="uppercase font-poppins font-semibold text-[#5E6282]">
-      {text}
-    </p>
-  );
+function SectionName({ text, uppercase }) {
+  let cssClasses = "font-poppins font-semibold text-[#5E6282]";
+  if (uppercase) {
+    cssClasses = "uppercase font-poppins font-semibold text-[#5E6282]";
+  }
+
+  return <p className={cssClasses}>{text}</p>;
 }
 
 export default SectionName;
