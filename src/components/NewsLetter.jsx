7be1c4107805debfab4c7af6e9706883658cda1d
Ahmed Aziz <azez55.ali@gmail.com>
2025-09-19 20:43:05 +0300

    fix: correct SVG attribute casing in NewsLetter component

--- src/components/NewsLetter.jsx
+++ src/components/NewsLetter.jsx
@@ -25,12 +25,12 @@ function NewsLetter() {
             xmlns="http://www.w3.org/2000/svg"
             fill="none"
             viewBox="0 0 24 24"
-            stroke-width="1"
+            strokeWidth="1"
             stroke="currentColor"
           >
             <path
-              stroke-linecap="round"
-              stroke-linejoin="round"
+              strokeLinecap="round"
+              strokeLinejoin="round"
               d="M3 8l9 6 9-6M4 6h16a2 2 0 012 2v8a2 2 0 01-2 2H4a2 2 0 01-2-2V8a2 2 0 012-2z"
             />
           </svg>
