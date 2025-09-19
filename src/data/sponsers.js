45b051897f1ec00eb12d4f25b5b3a91f2d8cf176
Ahmed Aziz <azez55.ali@gmail.com>
2025-09-19 19:04:21 +0300

    feat: add sponsers data for logo display

---
+++ src/data/sponsers.js
@@ -0,0 +1,22 @@
+export const sponsers = [
+  {
+    path: "/images/alitalia.png",
+    alt: "alitalia logo",
+  },
+  {
+    path: "/images/axon.png",
+    alt: "axon logo",
+  },
+  {
+    path: "/images/expedia.png",
+    alt: "expedia logo",
+  },
+  {
+    path: "/images/jetstar.png",
+    alt: "jetstar logo",
+  },
+  {
+    path: "/images/qantas.png",
+    alt: "qantas logo",
+  },
+];
