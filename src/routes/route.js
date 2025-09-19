61ec45c296d60dc40b583b6af70fa4337a7f5941
Ahmed Aziz <azez55.ali@gmail.com>
2025-09-20 00:44:15 +0300

    feat: add ErrorPage route for handling fake links

--- src/routes/route.js
+++ src/routes/route.js
@@ -1,10 +1,13 @@
 import { createBrowserRouter } from "react-router";
 import Home from "../pages/Home";
-
+import ErrorPage from "../pages/ErrorPage";
 const router = createBrowserRouter([
   {
     path: "/",
-    children: [{ index: true, Component: Home }],
+    children: [
+      { index: true, Component: Home },
+      { path: "fakeSocial", Component: ErrorPage },
+    ],
   },
 ]);
 
